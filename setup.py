d14b94de958322b5b9a041e5f2b61d2b7f352a9a
rlewa <95156390+rlewa@users.noreply.github.com>
2021-12-02 17:54:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Impact of news on predicting football matches",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/rlewa/projetofutebol/functions',
+    url='https://github.com/rlewa/projetofutebol',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
