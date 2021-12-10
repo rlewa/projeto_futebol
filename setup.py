619bf256b1c01aae4f6068be523570de3e826bd0
rlewa <95156390+rlewa@users.noreply.github.com>
2021-12-10 17:20:32 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['ftfy','wget', 'numpy', 'pandas', 'sklearn', 'nltk', 'pattern']
+    install_requires=['ftfy','wget', 'numpy', 'pandas', 'sklearn', 'nltk', 'pattern', 'unidecode', 'gensim', 'tensorflow', 'transformers']
 ) 
