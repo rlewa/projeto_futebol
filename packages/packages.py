e6285c10592e57fb6d252ea34bd1a232d8ae9fdd
rlewa <95156390+rlewa@users.noreply.github.com>
2021-12-10 22:03:31 -0300

    Update packages.py

--- packages/packages.py
+++ packages/packages.py
@@ -1,33 +1,32 @@
-import copy
-import ftfy
-import gensim
+import numpy as np
+import pandas as pd
+import scipy
+import pickle
+import random
+from random import choice
+import seaborn as sns
 import matplotlib.pyplot as plt
+
+import copy
+import string
+import regex as re
+from ftfy import fix_text
+import pattern
+from pattern.en import lemma, lexeme
+import ftfy
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 nltk.download('punkt')
 nltk.download('english')
 nltk.download('stopwords')
-import numpy as np
-import pandas as pd
-import pickle
-import random
-from random import choice
-import seaborn as sns
-import tensorflow as tf
-import time
-import numpy as np
-import pandas as pd
-from datetime import datetime, date
+
 import requests
 from bs4 import BeautifulSoup
+
 import dateutil.parser
-from google.colab import drive, files
-import string
-import regex as re
-from ftfy import fix_text
-import pattern
-from pattern.en import lemma, lexeme
+import time
+from datetime import datetime, date
 
 from tqdm import tqdm
 import unidecode
@@ -35,18 +34,18 @@ import re
 
 import xgboost as xgb
 
+import gensim
 from gensim.models import KeyedVectors, Word2Vec
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
-from IPython.display import SVG, clear_output
-from itertools import product
-
+import tensorflow as tf
 import tensorflow
 from tensorflow.keras import optimizers, regularizers
 from tensorflow.keras.constraints import unit_norm
 from tensorflow.keras.layers import Input, Flatten, LSTM, Conv1D, Dense, TimeDistributed, GlobalMaxPooling1D, Lambda
 from tensorflow.keras.models import Model, load_model, Sequential
 
+import sklearn
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
@@ -58,3 +57,7 @@ import transformers
 from transformers import AutoModel, AutoTokenizer, AutoConfig
 from transformers import BertForMaskedLM, BertModel, BertTokenizer
 from transformers import Trainer, TrainingArguments
+
+from google.colab import drive, files
+from IPython.display import SVG, clear_output
+from itertools import product
